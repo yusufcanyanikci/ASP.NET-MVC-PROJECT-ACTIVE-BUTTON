1aa1a4fcaf4293445cfcb5096c3bb40d37c86923
Yusufcan Yanıkcı <yupoyanucuk@gmail.com>
2023-06-01 20:02:45 +0300

    ASP.NET-MVC-PROJECT-ACTIVE-BUTTON

---
+++ ASP.NET ACTİVE BUTTON/README.md
@@ -0,0 +1,5 @@
+# asp-laslesVPN
+asp.net mvc project with modal
+
+![About](https://user-images.githubusercontent.com/126814579/227019921-a16ccb1e-e90d-4573-bf3e-1b55478d10cc.png)
+![Modal](https://user-images.githubusercontent.com/126814579/227019926-095d95f2-f2cb-48d9-bb22-2fa87e936e38.png)
---
+++ ASP.NET ACTİVE BUTTON/project/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using project.Models;
+
+namespace project.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Features()
+    {
+        return View();
+    }
+     public IActionResult Pricing()
+    {
+        return View();
+    }
+     public IActionResult Testimonials()
+    {
+        return View();
+    }
+     public IActionResult Help()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
